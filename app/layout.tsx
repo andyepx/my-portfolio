cd5121bf82639399296e72b460eb53f2e46b909a
Andrea Epifani <andrea@andreaepifani.com>
2024-04-23 10:22:28 +1000

    More attempts at fixing a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,10 @@ export default function RootLayout({
             )}
         >
         <head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
         </head>
         <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
